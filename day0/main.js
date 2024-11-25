becaf5f16d524fcb65b64cfe7722692db8a5b089
manojkumar-00 <manojkumar.ibmce@gmail.com>
2024-11-25 18:43:24 +0530

    Day Zero: code uploaded

---
+++ day0/main.js
@@ -0,0 +1,14 @@
+
+function test() {
+    if (true) {
+        var age = 30;
+    }
+    console.log(age);
+}
+
+test();
+
+var age = 20;
+let loc = "Kol";
+console.log("AGE: ", age);
+console.log("Location : ", loc)
